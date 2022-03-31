631c5cdf0746df0106cc011adf44d0b1332b331b
Maximilian Wagner <maw3350@gmail.com>
2022-03-31 16:52:34 -0400

    feat: API can return create/destoy data, all tests passing

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,4 +7,19 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params)), status: 201
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -41,4 +41,34 @@ describe "Items API" do
     expect(item[:attributes][:merchant_id]).to eq(merchant.id)
 
   end
+
+  it 'can create a new item' do
+    merchant = create(:merchant)
+    item_params = { name: 'Thing 1', unit_price: 5, description: 'the first thing', merchant_id: merchant.id}
+
+    post "/api/v1/items", params: item_params
+    expect(response).to be_successful
+
+    item = Item.last
+    expect(item.name).to eq 'Thing 1'
+    expect(item.unit_price).to eq 5
+    expect(item.description).to eq 'the first thing'
+    expect(item.merchant_id).to eq merchant.id
+  end
+
+  it 'can delete an item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+    id = item.id
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    expect(Item.count).to eq(0)
+  end
+
+  
 end
